316e6408ccf67e6d2372c6b88ca1084bc375ed06
mondy <workplacefocus@proton.me>
2025-09-17 18:05:28 +0300

    finish migrator

--- currency/cmd/migrator/main.go
+++ currency/cmd/migrator/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/BernsteinMondy/currency-service/currency/internal/config"
 	"github.com/BernsteinMondy/currency-service/currency/internal/migrations"
+	"github.com/spf13/viper"
 	"log"
 )
+
+type appConfig struct {
+	Database config.DatabaseConfig `mapstructure:"database"`
+}
 
 func main() {
 	err := run()
@@ -15,7 +21,7 @@ func main() {
 }
 
 func run() (err error) {
-	cfg, err := config.Load()
+	cfg, err := loadConfig()
 	if err != nil {
 		return fmt.Errorf("failed to load config: %w", err)
 	}
@@ -26,4 +32,30 @@ func run() (err error) {
 	}
 
 	return nil
+}
+
+func loadConfig() (appConfig, error) {
+	path := getPath()
+
+	var cfg appConfig
+	viper.SetConfigFile(path)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return cfg, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return cfg, fmt.Errorf("unable to unmarshal config: %w", err)
+	}
+
+	return cfg, nil
+}
+
+func getPath() string {
+	configPath := flag.String("config", "./config", "path to the config file")
+	flag.Parse()
+
+	return *configPath
 }
