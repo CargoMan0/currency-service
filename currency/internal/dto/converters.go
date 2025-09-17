e596a263d4cdd648924b4c1883c1d31fa4217352
mondy <workplacefocus@proton.me>
2025-09-17 18:24:02 +0300

    move requests struct to requests.go

--- currency/internal/dto/converters.go
+++ currency/internal/dto/converters.go
@@ -5,15 +5,6 @@ import (
 	"time"
 )
 
-const DefaultBaseCurrency = "RUB"
-
-type CurrencyRequestDTO struct {
-	BaseCurrency   string
-	TargetCurrency string
-	DateFrom       time.Time
-	DateTo         time.Time
-}
-
 func CurrencyRequestFromPbToDTO(req *currency.GetRateRequest, baseCurrency string) *CurrencyRequestDTO {
 	return &CurrencyRequestDTO{
 		BaseCurrency:   baseCurrency,
--- currency/internal/dto/requests.go
+++ currency/internal/dto/requests.go
@@ -1,1 +1,12 @@
 package dto
+
+import "time"
+
+const DefaultBaseCurrency = "RUB"
+
+type CurrencyRequestDTO struct {
+	BaseCurrency   string
+	TargetCurrency string
+	DateFrom       time.Time
+	DateTo         time.Time
+}
